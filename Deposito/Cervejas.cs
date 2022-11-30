19e7071a80d9c77d8883b8f195e01fa8de6ea8c8
Irene Egler <irene.egler@gmail.com>
2022-11-30 10:30:39 -0300

    Classe Tipo de Cerveja

---
+++ Deposito/Cervejas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deposito
+{
+    internal class Cervejas
+    {
+        string 
+        Lager,
+        Pilsen,
+        PuroMalte,
+        Escura,
+        ZeroAlcool;
+    }
+}
