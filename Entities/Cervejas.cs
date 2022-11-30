1baadff3a02d08b64ab4f6a05858ea3503471bed
Irene Egler <irene.egler@gmail.com>
2022-11-30 11:04:05 -0300

    Entities

---
+++ Entities/Cervejas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deposito.Entities
+{
+    internal class Cervejas
+    {
+        public int Tipos { get; set; }
+public OrderStatus Status { get; set; }
+
+enum OrderStatus : int {
+        Lager = 1,
+        Pilsen = 2,
+        PuroMalte = 3,
+        Escura = 4,
+        ZeroAlcool = 5
+    }
+}
+}
